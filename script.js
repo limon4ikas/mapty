be41f8fdded57a5d57a46bf6a8805c46e191a02e
limon4ikas <limon4ikas@gmail.com>
2021-01-12 13:15:52 +0300

    adding markers to map

--- script.js
+++ script.js
@@ -17,21 +17,37 @@ if (navigator.geolocation) {
     function (position) {
       console.log(position);
 
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      const coords = [latitude, longitude];
+      // Getting user coordinates
+      const { latitude, longitude } = position.coords;
+      const userCoords = [latitude, longitude];
 
-      const map = L.map('map').setView(coords, 13);
-
+      // Adding map
+      const map = L.map('map').setView(userCoords, 13);
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+      // Event listener
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+
+        // Adding marker to map
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 100,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
     },
     function () {
       alert('Could not get your position');
